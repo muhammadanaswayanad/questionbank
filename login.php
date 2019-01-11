f375cce2ee72b840a6b6c6f43a1f8822f8d67def
Muhammad Anas <muhammadanaswayanad@gmail.com>
2019-01-11 20:43:18 +0530

    Initial

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="csscode.css">
+	<link rel="javascript" type="text/javascript" href="jscode.js">
+</head>
+<body>
+<?php
+
+$db=mysql_connect("localhost","root","");
+if($db)
+{
+ mysql_select_db("questionbank",$db);
+ 
+ }
+ else {
+  
+  echo("Error! Unable to establish connection.");
+  
+  }
+?>
+<div class="login-page">
+  <div class="form">
+  	<!--We dont need registration screen so commenting the code
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+-->
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</body>
+</html>
---
+++ logincontroller.php
@@ -0,0 +1,46 @@
+<?php
+
+$db=mysql_connect("localhost","root","");
+if($db)
+{
+ mysql_select_db("questionbank",$db);
+ 
+ }
+ else {
+  
+  echo("Error! Unable to establish connection.");
+  
+  }
+?>
+<?php
+ $un=$_POST["username"];
+ $pass=$_POST["password"];
+ $re=mysql_query("select * from login where username='$un' and password='$pass'");
+ if(mysql_num_rows($re)>0)
+ {
+  $row=mysql_fetch_array($re);
+  $id=$row[0];
+  session_start();
+  $_SESSION["lid"]=$id;
+  
+  $type=$row[3];
+  if($type=="admin")
+  {
+   header("location:adminhome.php");
+  }
+  else if($type=="staff")
+  {
+   header("location:staffhome.php");
+  }
+  else if($type=="user")
+  {
+   header("location:userhome.php");
+  }
+  
+ }
+ else
+  {echo "invalid credentials";
+   } 
+}
+
+?>
