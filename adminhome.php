3d34dfafe3b7b42949083dc7fefc5037810d805b
Muhammad Anas <muhammadanaswayanad@gmail.com>
2019-02-11 15:52:39 +0530

    adminhome

--- adminhome.php
+++ adminhome.php
@@ -7,3 +7,23 @@ else{
 	header("location:login.php");
 }
 ?>
+<html>
+<head>
+<title>
+Question Bank :: Admin Home
+</title>
+</head>
+<body>
+<?php
+
+include("adminheader.php");
+?>
+
+<form action="" method="get">
+  
+</form>
+<?php
+include("adminfooter.php");
+?>
+</body>
+</html>
